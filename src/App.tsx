39b9e0b1bcf234c443576f66cc41078cbc46085f
alanlgoncalves <alan.lgoncalves@outlook.com>
2020-06-04 21:56:57 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 32 }}>Marvel App!</Text>
+    </View>
+  );
+};
+
+export default App;
