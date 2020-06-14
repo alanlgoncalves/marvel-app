2c448b58cc12ad82f276a14b408e413fc848d596
alanlgoncalves <alan.lgoncalves@outlook.com>
2020-06-14 10:28:52 -0300

    Update api-key config file

--- src/config/api-key.ts
+++ src/config/api-key.ts
@@ -2,12 +2,8 @@ import md5 from 'md5';
 
 const apiKey = {
   ts: 'marvel-api',
-  apikey: '607702306e1d5a4caa024f7aaa3ee1eb',
-  hash: md5(
-    'marvel-api' +
-      '2e74267cc49e590b8a90aadab3b02bdbb1c84d8c' +
-      '607702306e1d5a4caa024f7aaa3ee1eb',
-  ),
+  apikey: 'public_key',
+  hash: md5('marvel-api' + 'private_key' + 'public_key'),
 };
 
 export default apiKey;
